7cba829724dc9edf859516570f970c73ed2f5e3e
Vit4h <wjherrera@correo.url.edu.gt>
2023-09-06 23:46:59 -0600

    creando menu de inicio

--- Main.java
+++ Main.java
@@ -1,6 +1,48 @@
+import java.util.Scanner;
 
 public class Main{
+    
+    static void menu(){
+        try {
+                new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
+            } catch (Exception e) {
+                /*No hacer nada*/
+            }
+               /*Introduce tu código desde aquí*/
+        Scanner objeto = new Scanner(System.in);
+        System.out.println("Laboratorio 1");
+        System.out.println("Walter Herrera 1109722");
+        System.out.println("1.Insertar registro");
+        System.out.println("2.Eliminar Registro");
+        System.out.println("3.Buscar usuario");
+        System.out.println("4. salir");
+        int opcion = objeto.nextInt();
+        switch(opcion){
+            case 1:
+
+            break;
+
+            case 2:
+
+            break;
+
+            case 3:
+
+            break;
+
+            case 4:
+                System.out.println("Feliz dia");
+                System.out.close();
+            break;
+
+            default:
+                System.out.println("Valor no aceptado intente nuevamente");
+                menu();
+            break;
+        }
+
+    }
     public static void main(String[] args) {
-        System.out.println("Hola mundo");
+        menu();
     }
 }
